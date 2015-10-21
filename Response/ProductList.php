29226a18bde18246ba5e1258d8f0dc8e72f4d53e
Matt Kowalski <mat.kowalski@gmail.com>
2015-10-21 14:13:31 +0200

    Fetch products with use of Service Contracts

--- Response/ProductList.php
+++ Response/ProductList.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace MeetMagentoPL\IntegrationAbstraction\Response;
+
+
 /**
  * Description of ProductList
  *
@@ -9,12 +11,44 @@ namespace MeetMagentoPL\IntegrationAbstraction\Response;
 class ProductList implements ResponseInterface
 {
     /**
+     * @var \Magento\Framework\Api\SearchCriteriaBuilder
+     */
+    protected $searchCriteriaBuilder;
+
+    /**
+     * @var \Magento\Catalog\Api\ProductRepositoryInterface
+     */
+    protected $productRepository;
+
+    /**
+     * @param \Magento\Catalog\Api\ProductRepositoryInterface $productRepository
+     */
+    public function __construct(
+        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
+        \Magento\Framework\Api\SearchCriteriaBuilder $searchCriteriaBuilder
+
+    )
+    {
+        $this->productRepository = $productRepository;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+    }
+
+    /**
      * 
      * @param array $params
      * @return array|mixed
      */    
     public function execute(array $params)
     {
+
+        $searchCriteria = $this->searchCriteriaBuilder->create();
+
+        $productList = $this->productRepository->getList($searchCriteria);
+
+        foreach($productList->getItems() as $product) {
+            
+        }
+
         /** @todo implementation */
         return [['produkt 1', 13.88],['produkt 2', 10.22]];
     }
